7c4c8a174694ca1f5d516e737ca377ab1b698b1c
Sameer-kumar-708 <sameer.tmv2005@gmail.com>
2026-01-05 23:47:19 +0530

    Sort

---
+++ DSA/Insertion_Sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  int arr[] = {2, 5, 1, 8, 3, 9, 0, 4};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  for (int i = 1; i < n; i++)
+  {
+    for (int j = i; j > 0; j--)
+    {
+      if (arr[j] > arr[j - 1])
+      {
+        swap(arr[j], arr[j - 1]);
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << ' ';
+  }
+
+  return 0;
+}
--- DSA/Selection_Sort.cpp
+++ DSA/Selection_Sort.cpp
@@ -26,7 +26,7 @@ int main()
 
     for (int j = i + 1; j < n; j++)
     {
-      if (arr[j] > arr[idx])
+      if (arr[j] < arr[idx])
       {
         idx = j;
       }
